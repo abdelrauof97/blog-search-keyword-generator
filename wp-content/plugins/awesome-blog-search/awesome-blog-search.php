2c4bc7a06071aa0240da9b5d029bdfcb69c25a59
Abdelrauof <abdelrauof97@gmail.com>
2019-01-05 06:15:10 +0200

    added the blog search plugin

---
+++ wp-content/plugins/awesome-blog-search/awesome-blog-search.php
@@ -0,0 +1,17 @@
+<?php
+   /*
+   Plugin Name: Awesomeness Blog Search
+   Plugin URI: http://wwww.abdelrauof.me
+   description: >-
+  a plugin to create awesomeness search for blogs in the website
+   Version: 1.2
+   Author: Abdelrauof Mohammed
+   Author URI: http://wwww.abdelrauof.me
+   License: GPL2
+   */
+
+
+
+
+
+?>
